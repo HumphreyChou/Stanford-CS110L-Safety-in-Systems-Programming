583cebbce6f474899cbe362ff363e7096fda5663
HumphreyChou <zhouyuhan_@outlook.com>
2022-01-02 18:41:32 +0800

    finish week6

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,46 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    for _ in 0..input_vec.len() {
+        // This is really silly but I could not find a better way
+        output_vec.push(Default::default());
+    }
+    let batch = input_vec.len() / num_threads + 1;
+    let (tx, rx) = crossbeam_channel::unbounded();
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let mut nums = Vec::new();
+        for _ in 0..batch {
+            match input_vec.pop() {
+                Some(num) => nums.push((input_vec.len(), num)),
+                None => break
+            };
+        }
+        if nums.len() == 0 {
+            break;
+        }
+        let sender = tx.clone();
+        threads.push(thread::spawn(move || {
+            loop {
+                match nums.pop() {
+                    Some((index, num)) => sender
+                        .send((index, f(num)))
+                        .expect("Sender error in worker threads"),
+                    None => break,
+                }
+            }
+            // drop(sender);
+        }));
+    }
+    drop(tx);
+
+    while let Ok((index, res)) = rx.recv() {
+        output_vec[index] = res;
+    }
+
+    for handle in threads {
+        handle.join().expect("Panics when reaping child threads");
+    }
     output_vec
 }
 
